5a4ffe6e92bed0602965023de61dc37e279b8bec
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2018-02-15 19:20:01 +0100

    Actividad 0092, modificadas las respuestas, implementada la clase HashMap

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.HashMap;
 
 /**
  * The responder class represents a response generator object.
@@ -12,7 +13,7 @@ public class Responder
 {
     ArrayList<String> respuestas;
     Random aleatorio;
-
+    HashMap<String, String> respuestasForInput;
     /**
      * Construct a Responder - nothing to do
      */
@@ -20,7 +21,13 @@ public class Responder
     {
         respuestas = new ArrayList<>(); 
         aleatorio = new Random();
-
+        respuestasForInput = new HashMap<>();
+        
+        respuestasForInput.put("payment", "Our payment service is down right now, please wait");
+        respuestasForInput.put("aq", "You can ask questions to us and we response you later.");
+        respuestasForInput.put("linux", "Our software allready can be runned on linux.");
+        respuestasForInput.put("help", "Follow this page if you need our help.");
+        
         respuestas.add("Give me a second to solve the problem please.");
         respuestas.add("Thanks you for your time.");
         respuestas.add("Thanks you for your help.");
@@ -32,8 +39,15 @@ public class Responder
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String input)
     {
-        return respuestas.get(aleatorio.nextInt(respuestas.size()));
+        String respuesta = null;
+        respuesta = respuestasForInput.get(input);
+        
+        if(respuesta == null){
+            respuesta = respuestas.get(aleatorio.nextInt(respuestas.size()));
+        }
+        
+        return respuesta;
     }
 }
